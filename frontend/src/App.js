15a109348b1ad0d9c6ae5113bb5e0d8258f07de5
shiv kumar <kumarshiv9031127789@gmail.com>
2024-02-16 18:23:42 +0530

    delete component file

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,5 @@
-import logo from './logo.svg';
-import './App.css';
-import { Header } from './component/Header.jsx';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
@@ -19,7 +18,6 @@ function App() {
           Learn React
         </a>
       </header>
-      <Header/>
     </div>
   );
 }
